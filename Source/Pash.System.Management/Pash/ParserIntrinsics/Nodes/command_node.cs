af96ebb0abf8900c8a4c693eabb3395605333f9e
Jay Bazuzi <jay@bazuzi.com>
2012-08-24 17:28:47 -0700

    FORMATTING

--- Source/Pash.System.Management/Pash/ParserIntrinsics/Nodes/command_node.cs
+++ Source/Pash.System.Management/Pash/ParserIntrinsics/Nodes/command_node.cs
@@ -22,41 +22,41 @@ namespace Pash.ParserIntrinsics.Nodes
 
         internal override object Execute(ExecutionContext context, ICommandRuntime commandRuntime)
         {
-                var commandText = parseTreeNode.ChildNodes[0].FindTokenAndGetText();
-                CommandInfo commandInfo = ((LocalRunspace)context.CurrentRunspace).CommandManager.FindCommand(commandText);
+            var commandText = parseTreeNode.ChildNodes[0].FindTokenAndGetText();
+            CommandInfo commandInfo = ((LocalRunspace)context.CurrentRunspace).CommandManager.FindCommand(commandText);
 
-                if (commandInfo == null)
-                    throw new InvalidOperationException(commandText);
+            if (commandInfo == null)
+                throw new InvalidOperationException(commandText);
 
-                IEnumerable<CommandParameter> parameters = new CommandParameter[]{};
-                if (parseTreeNode.ChildNodes.Count == 2)
+            IEnumerable<CommandParameter> parameters = new CommandParameter[] { };
+            if (parseTreeNode.ChildNodes.Count == 2)
+            {
+                var commandElementsNode = (command_elements_node)parseTreeNode.ChildNodes[1].AstNode;
+                parameters = (IEnumerable<CommandParameter>)commandElementsNode.Execute(context, commandRuntime);
+            }
+
+            // MUST: fix this with the commandRuntime
+            Pipeline pipeline = context.CurrentRunspace.CreateNestedPipeline();
+
+            // TODO: Fill the pipeline with input data?
+            //pipeline.Input.Write(context.inputStreamReader);
+
+            context.PushPipeline(pipeline);
+
+            try
+            {
+                var command = new Command(commandText);
+                foreach (var parameter in parameters)
                 {
-                    var commandElementsNode = (command_elements_node)parseTreeNode.ChildNodes[1].AstNode;
-                    parameters = (IEnumerable<CommandParameter>)commandElementsNode.Execute(context, commandRuntime);
+                    command.Parameters.Add(parameter);
                 }
-
-                // MUST: fix this with the commandRuntime
-                Pipeline pipeline = context.CurrentRunspace.CreateNestedPipeline();
-
-                // TODO: Fill the pipeline with input data?
-                //pipeline.Input.Write(context.inputStreamReader);
-
-                context.PushPipeline(pipeline);
-
-                try
-                {
-                    var command = new Command(commandText);
-                    foreach (var parameter in parameters)
-                    {
-                        command.Parameters.Add(parameter);
-                    }
-                    pipeline.Commands.Add(command);
-                    return pipeline.Invoke();
-                }
-                finally
-                {
-                    context.PopPipeline();
-                }
+                pipeline.Commands.Add(command);
+                return pipeline.Invoke();
+            }
+            finally
+            {
+                context.PopPipeline();
+            }
 
         }
     }
