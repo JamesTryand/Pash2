8f234c633783ad864ce0e375736818668c616705
Jay Bazuzi <jay@bazuzi.com>
2012-08-26 13:37:47 -0700

    Acceptance tests (in NUnit)
    Using a mock PSHost (instead of the console) we can run arbitrary
    PowerShell statements and examine the results.

---
+++ Source/AcceptanceTests/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace AcceptanceTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // put debuggable test code here.
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Source/AcceptanceTests/Tests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+
+namespace AcceptanceTests
+{
+    [TestFixture]
+    class Tests
+    {
+        [Test]
+        public void VerbaitmString()
+        {
+            VerifyExpression("xxx", "'xxx'");
+        }
+
+        [Test]
+        public void AddStringToIntegerTest()
+        {
+            VerifyExpression("xxx1", "'xxx' + 1");
+        }
+
+        [Test]
+        public void AddIntegersTest()
+        {
+            VerifyExpression(3, "1 + 2");
+        }
+
+        static void VerifyExpression<T>(T expected, string expression)
+        {
+            var x = Execute(expression);
+            Assert.AreEqual(expected, GetPSObjectSingleton<T>(x));
+        }
+
+        static T GetPSObjectSingleton<T>(Collection<PSObject> x)
+        {
+            return (T)(x.Single().ImmediateBaseObject);
+        }
+
+        static Collection<PSObject> Execute(string statement)
+        {
+            var myHost = new TestHost();
+            var myRunSpace = RunspaceFactory.CreateRunspace(myHost);
+            myRunSpace.Open();
+
+            using (var currentPipeline = myRunSpace.CreatePipeline())
+            {
+                currentPipeline.Commands.Add(statement);
+                return currentPipeline.Invoke();
+            }
+        }
+    }
+}
