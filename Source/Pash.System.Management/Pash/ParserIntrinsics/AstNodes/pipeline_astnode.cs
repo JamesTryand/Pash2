e65add88e0213ca1b74eb24a0161fb692ec38a5b
Jay Bazuzi <jay@bazuzi.com>
2012-08-27 11:38:00 -0700

    Trivial pipeline implementation.
    Only works with single-element pipelines, e.g.
       PS> Get-ChildItem

---
+++ Source/Pash.System.Management/Pash/ParserIntrinsics/AstNodes/pipeline_astnode.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Irony.Ast;
+using Irony.Parsing;
+using Pash.Implementation;
+using System.Management.Automation.Runspaces;
+using System.Collections.ObjectModel;
+using System.Management.Automation;
+
+namespace Pash.ParserIntrinsics.AstNodes
+{
+    public class pipeline_astnode : _astnode
+    {
+        public pipeline_astnode(AstContext astContext, ParseTreeNode parseTreeNode)
+            : base(astContext, parseTreeNode)
+        {
+        }
+
+        internal override object Execute(ExecutionContext context, ICommandRuntime commandRuntime)
+        {
+            if (this.parseTreeNode.ChildNodes.Count == 1) { return ((_astnode)this.parseTreeNode.ChildNodes.Single().AstNode).Execute(context, commandRuntime); }
+
+            throw new NotImplementedException();
+        }
+    }
+}
